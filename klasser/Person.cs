705423718a097b9af176001679b7c231c062cc60
Liza Odish <lizaodishl@gmail.com>
2023-02-13 18:05:08 +0100

    Add project files.

---
+++ klasser/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Klasser
+{
+    public class Person
+    {
+        private string name;
+        private string surname;
+        private long personnumber;
+
+        public Person() { }
+        public Person(string namn, string efternamn, long personnummer)
+        {
+            this.name = namn;
+            this.surname = efternamn;
+            this.personnumber = personnummer;
+        }
+        public void setnamn(string _name)
+        {
+            this.name = _name;
+        }
+        public void setsurname(string _surname)
+        {
+            this.name = _surname;
+        }
+        public void setperson_nr(long _personnumber)
+        {
+            this.personnumber = _personnumber;
+        }
+
+        public string getnamn()
+        {
+            return name;
+        }
+
+        public string getsurname()
+        {
+            return surname;
+        }
+        public long getpersonnumber()
+        {
+            return personnumber;
+        }
+
+        public string ToString()
+        {
+            string minperson = "";
+            minperson = "name: " + name + "\nsurname: " + surname + "\npersonnumber:" + personnumber;
+            return minperson;
+        }
+
+    }
+}
+
+
+
---
+++ klasser/Program.cs
@@ -0,0 +1,19 @@
+﻿using klasser;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Person person1;
+        person1 = new Person();
+        Person person2= new Person();
+        Person person3= new Person("Liza" , "Odish", 123456789);
+
+        person1.setname("Milo");
+        person1.setlastname("Plupp");
+        person1.setperson_nr(2001012);
+        Console.WriteLine(person3.getname());
+        Console.WriteLine(person3.ToString());
+    }
+}
